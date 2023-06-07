13a8c261234a9c69ff855bb956733b67b061840a
Miracle <miraclepatidehen@gmail.com>
2023-06-07 20:45:22 +0100

    updated

--- streemHubClient/src/components/NavBar.jsx
+++ streemHubClient/src/components/NavBar.jsx
@@ -1,4 +1,3 @@
-import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
@@ -11,7 +10,7 @@ const NavBar = () => {
   return (
     <nav className="py-10 px-20 flex items-center gap-6">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-14" />
+        <img src="/logo.svg" alt="logo" className="w-14" />
       </Link>
       {navLinks.map(({ link, text }, i) => (
         <Link to={link} className="text-xl capitalize" key={i}>
