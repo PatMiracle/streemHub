de7e727941c4a13daeddd248a5e765dca52b3a58
Miracle <miraclepatidehen@gmail.com>
2023-06-07 16:07:20 +0100

    Added footer component

---
+++ streemHubClient/src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Footer = () => {
+  // temp data
+  const data = [
+    {
+      category: 'products',
+      list: ['pricing', 'features', 'customers', 'feedback'],
+    },
+    {
+      category: 'help',
+      list: ['getting started', 'sign up', 'login', 'FAQ'],
+    },
+    {
+      category: 'site map',
+      list: ['ABOUT US', 'COLLAB', 'POSTS', 'CONTACT US'],
+    },
+  ]
+
+  return (
+    <footer className="bg-black text-white p-32 flex justify-between">
+      {data.map(({ category, list }, i) => {
+        return (
+          <div key={i}>
+            <h3 className="font-bold uppercase text-2xl">{category}</h3>
+            <ul className="flex flex-col gap-6 pt-6 capitalize">
+              {list.map((text, i) => (
+                <li key={i} className="text-xl">
+                  {text}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )
+      })}
+      <div>
+        <h3 className="font-bold uppercase text-2xl">subscribe</h3>
+        <form>
+          <input
+            type="text"
+            className="rounded-md text-black py-2 px-3 mt-6"
+            placeholder="Enter email address"
+          />
+          <button className="bg-purple-600 block mt-4 ml-auto py-1 px-3 rounded-md">
+            Send
+          </button>
+        </form>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- streemHubClient/src/pages/Home.jsx
+++ streemHubClient/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <>
+      <Footer />
+    </>
+  )
 }
 
 export default Home
