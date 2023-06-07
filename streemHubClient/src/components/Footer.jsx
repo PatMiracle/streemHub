52d5804dc520d02138bdbd123d73a7e1e844e493
Miracle <miraclepatidehen@gmail.com>
2023-06-07 18:33:51 +0100

    email validation added

--- streemHubClient/src/components/Footer.jsx
+++ streemHubClient/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Footer = () => {
   // temp data
@@ -16,6 +16,25 @@ const Footer = () => {
       list: ['ABOUT US', 'COLLAB', 'POSTS', 'CONTACT US'],
     },
   ]
+
+  const [email, setEmail] = useState('')
+  const emailRef = useRef(null)
+
+  //   clear emailRef when email changes
+  useEffect(() => {
+    emailRef.current.textContent = ''
+  }, [email])
+
+  const handleEmailSubmit = (e) => {
+    e.preventDefault()
+    // validate email
+    const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+    if (!email.match(emailRegex)) {
+      emailRef.current.textContent = 'invalid email'
+    } else {
+      emailRef.current.textContent = 'subscribed'
+    }
+  }
 
   return (
     <footer className="bg-black text-white p-32 flex justify-between">
@@ -35,12 +54,16 @@ const Footer = () => {
       })}
       <div>
         <h3 className="font-bold uppercase text-2xl">subscribe</h3>
-        <form>
+        <form onSubmit={handleEmailSubmit}>
           <input
             type="text"
             className="rounded-md text-black py-2 px-3 mt-6"
             placeholder="Enter email address"
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
           />
+          <p className="text-purple500 text-xs" ref={emailRef}></p>
           <button className="bg-purple500 block mt-4 ml-auto py-1 px-4 text-sm rounded-md">
             Send
           </button>
