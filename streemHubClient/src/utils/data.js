c986647a225495f6e7706360cfa81bb4c8645974
Miracle <miraclepatidehen@gmail.com>
2023-06-13 06:42:02 +0100

    added temporary posts data

---
+++ streemHubClient/src/utils/data.js
@@ -0,0 +1,27 @@
+// temp posts data
+export const posts = [
+  {
+    id: 1,
+    name: 'Bob Willer',
+    text: 'Can you make changes to a product which other developers have already started?',
+    img: true,
+  },
+  {
+    id: 2,
+    name: 'Bob Willer',
+    text: "🚀 Building the future, one line of code at a time! Excited to share the latest project I've been working on. Stay tuned for the unveiling! 💻✨ #DeveloperLife #CodeCrafting #InnovationInProgress",
+    img: false,
+  },
+  {
+    id: 3,
+    name: 'Bob Willer',
+    text: 'Can you make changes to a product which other developers have already started?',
+    img: true,
+  },
+  {
+    id: 4,
+    name: 'Bob Willer',
+    text: 'Can you make changes to a product which other developers have already started?',
+    img: true,
+  },
+]
