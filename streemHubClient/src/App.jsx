d6b621009005ba70e2ec514211681caeb0242d87
Miracle <miraclepatidehen@gmail.com>
2023-06-17 18:27:27 +0100

    Added verify email page

--- streemHubClient/src/App.jsx
+++ streemHubClient/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Posts from './pages/Posts'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import VerifyEmail from './pages/VerifyEmail'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="posts" element={<Posts />} />
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
+        <Route path="verify-email" element={<VerifyEmail />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ streemHubClient/src/pages/VerifyEmail.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const VerifyEmail = () => {
+  return (
+    <main>
+      <img
+        src="/verify_img.png"
+        alt=""
+        className="fixed h-screen top-0 left-0"
+      />
+      <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]">
+        <h2 className="text-5xl font-bold text-left">Verify your email</h2>
+        <p className="text-xl py-6">
+          Please enter the 4 digit code sent to yourmail@example.com
+        </p>
+        <div className="flex gap-5">
+          <input
+            type="text"
+            className="bg-[#D9D9D9] text-2xl text-center py-5 w-[131px] rounded-lg"
+            maxLength={1}
+          />
+          <input
+            type="text"
+            className="bg-[#D9D9D9] text-2xl text-center py-5 w-[131px] rounded-lg"
+            maxLength={1}
+          />
+          <input
+            type="text"
+            className="bg-[#D9D9D9] text-2xl text-center py-5 w-[131px] rounded-lg"
+            maxLength={1}
+          />
+          <input
+            type="text"
+            className="bg-[#D9D9D9] text-2xl text-center py-5 w-[131px] rounded-lg"
+            maxLength={1}
+          />
+        </div>
+        <button className="text-purple500 text-2xl pt-14 pb-7">
+          Resend Code
+        </button>
+        <button className="font-bold bg-purple500 block text-2xl w-[580px] py-4 rounded-lg">
+          Confirm
+        </button>
+        <Link
+          to="/signup"
+          className="text-purple500 text-2xl pt-4 block text-center underline"
+        >
+          Change Email
+        </Link>
+      </div>
+    </main>
+  )
+}
+
+export default VerifyEmail
