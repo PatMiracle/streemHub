34a908f60fe3bf4acc04f46e1df9a013e8aabb00
PatMiracle <112787220+PatMiracle@users.noreply.github.com>
2023-06-17 21:54:43 +0100

    Update SignUp.jsx

--- streemHubClient/src/pages/SignUp.jsx
+++ streemHubClient/src/pages/SignUp.jsx
@@ -25,7 +25,7 @@ const SignUp = () => {
     if (
       !name_regex.test(firstName) ||
       !name_regex.test(lastName) ||
-      email_regex.test(email)
+      !email_regex.test(email)
     ) {
       setFormError(true)
     }
