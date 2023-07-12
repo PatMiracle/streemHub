2db257fc7cab69366e80c89b146733976cb2421a
Miracle <miraclepatidehen@gmail.com>
2023-07-12 12:55:15 +0100

    added changes

--- streemHubClient/src/App.jsx
+++ streemHubClient/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import Posts from './pages/Posts'
 import Login from './pages/Login'
@@ -6,19 +6,15 @@ import SignUp from './pages/SignUp'
 import VerifyEmail from './pages/VerifyEmail'
 import Verified from './pages/Verified'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="posts" element={<Posts />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="verify-email" element={<VerifyEmail />} />
-        <Route path="verified" element={<Verified />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
+const routes = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/posts', element: <Posts /> },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+  { path: '/verify-email', element: <VerifyEmail /> },
+  { path: '/verified', element: <Verified /> },
+])
+
+const App = () => <RouterProvider router={routes} />
 
 export default App
