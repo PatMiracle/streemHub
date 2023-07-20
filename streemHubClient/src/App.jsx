81d12d6c5a163eec3974c0f4a8f78560b0b1a591
Miracle <miraclepatidehen@gmail.com>
2023-07-20 16:57:02 +0100

    updated paths and blocked mobile view

--- streemHubClient/src/App.jsx
+++ streemHubClient/src/App.jsx
@@ -5,6 +5,8 @@ import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import VerifyEmail from './pages/VerifyEmail'
 import Verified from './pages/Verified'
+import DashboardLayout from './components/DashboardLayout'
+import Profile from './pages/Profile'
 
 const routes = createBrowserRouter([
   { path: '/', element: <Home /> },
@@ -13,8 +15,25 @@ const routes = createBrowserRouter([
   { path: '/signup', element: <SignUp /> },
   { path: '/verify-email', element: <VerifyEmail /> },
   { path: '/verified', element: <Verified /> },
+  {
+    path: '/',
+    element: <DashboardLayout />,
+    children: [{ path: '/profile', element: <Profile /> }],
+  },
 ])
 
-const App = () => <RouterProvider router={routes} />
+const App = () => {
+  return (
+    <>
+      <div className="bg-white fixed top-0 left-0 min-h-screen z-40 w-full lg:hidden block">
+        <h3 className="text-black">
+          Hey there StreemHub is currently unavailable for mobile devices
+        </h3>
+        <h3 className="text-black">Please swicth to desktop</h3>
+      </div>
+      <RouterProvider router={routes} />
+    </>
+  )
+}
 
 export default App
