378ccbd86cda2855438a7067abf9fcff5a8f2dc7
Miracle <miraclepatidehen@gmail.com>
2023-08-04 14:05:14 +0100

    updated input field

--- streemHubClient/src/pages/Posts/Main.jsx
+++ streemHubClient/src/pages/Posts/Main.jsx
@@ -7,12 +7,17 @@ const Main = () => {
     <main className="py-48 w-[85%] mx-auto">
       <div className="px-10 py-5 border-4 border-purple500 drop-shadow-md rounded-[30px] flex justify-between">
         <div className="w-14 h-14 bg-purple500 rounded-full"></div>
+        <input
+          type="text"
+          placeholder="Share your Problems and Ideas"
+          className="w-10/12 bg-black text-white text-xl px-3"
+        />
         <FaMicrophone className="text-purple500 opacity-75" size={56} />
       </div>
       {/* posts */}
       <section className="pt-24 pb-10">
-        {posts.map(({ id, name, text, img }) => (
-          <Post key={id} name={name} text={text} img={img} />
+        {posts.map((data, i) => (
+          <Post key={i} {...data} />
         ))}
       </section>
       <div className="flex w-max gap-6 ml-auto mr-[9%]">
