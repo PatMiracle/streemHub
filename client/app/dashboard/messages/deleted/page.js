7ea3125a5240af811cecaaec155f0ccc949771f1
Miracle <miraclepatidehen@gmail.com>
2023-09-25 17:43:18 +0100

    added dashboard pages

---
+++ client/app/dashboard/messages/deleted/page.js
@@ -0,0 +1,5 @@
+const DeletedMessages = () => {
+  return <div>DeletedMessages</div>
+}
+
+export default DeletedMessages
---
+++ client/app/dashboard/messages/starred/page.js
@@ -0,0 +1,5 @@
+const StarredMessages = () => {
+  return <div>StarredMessages</div>
+}
+
+export default StarredMessages
