bf7a98cdabc9f09f6906b4e9afecc8db698e1057
Miracle <miraclepatidehen@gmail.com>
2023-09-30 12:57:12 +0100

    added new auth pages

---
+++ client/app/login/change-password/page.js
@@ -0,0 +1,12 @@
+import AuthLayout from '/layouts/auth-layout'
+import img from '/public//images/login-img.png'
+
+const Login = () => {
+  return (
+    <AuthLayout img={img}>
+      <form></form>
+    </AuthLayout>
+  )
+}
+
+export default Login
